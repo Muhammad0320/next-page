9fde5bfa05fe905f7e237d520fce216a2e5856e0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-06 14:01:50 +0100

    chore: initial commit

---
+++ pages/about.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+      <h1> I'm in the about page </h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+function Index() {
+  return (
+    <div>
+      <h1> This is the Index route </h1>
+    </div>
+  );
+}
+a;
+
+export default Index;
