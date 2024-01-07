275fbf21f4d837271bd32debab93c0f80cbb63d4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-07 11:21:57 +0100

    chore: update all shit comp

--- pages/blog/[...allShitGoesHere].jsx
+++ pages/blog/[...allShitGoesHere].jsx
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 function AllFileCpmp() {
+  const router = useRouter();
+
+  console.log(router.query);
+
   return (
     <div>
       <h1> Anthing can go here </h1>
