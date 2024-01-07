ac940505cd885d715fc0c40e716eebda9fba35ce
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-07 14:58:02 +0100

    chore: update index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,24 @@
 import Link from "next/link";
 
 function Index() {
+  const clients = [
+    { name: "maximillian", id: "max" },
+    { name: "Muhammad", id: "SAW" },
+  ];
+
   return (
     <div>
       <h1> This is the Index route </h1>
 
       <ul>
-        <li>
-          {" "}
-          <Link href="/blog">Blog</Link>{" "}
-        </li>
-
-        <li>
-          <Link href="/blog/shit/dumb"> Place of ... </Link>
-        </li>
+        {clients.map((cl) => (
+          <li key={cl.id}>
+            <Link href={{ pathname: "/client/[id]", query: { id: cl.id } }}>
+              {" "}
+              {cl.name}{" "}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
